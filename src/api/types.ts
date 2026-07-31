849f2c8e1789d117cdc5f537fb7b46829857df53
Nazar Krughliakov <zonnix336@gmail.com>
2026-07-31 13:44:25 +0300

    add TMDB client with movie and genre requests

---
+++ src/api/types.ts
@@ -0,0 +1,33 @@
+export interface Movie {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    title: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    softcore: boolean;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+export interface Genre {
+    id: number;
+    name: string;
+}
+
+export interface GenresResponse {
+    genres: Genre[];
+}
+
+export interface TmdbListResponse<T, > {
+    page: number,
+    results: T[],
+    total_pages: number,
+    total_results: number
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,4 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
     <App/>
 )
+
